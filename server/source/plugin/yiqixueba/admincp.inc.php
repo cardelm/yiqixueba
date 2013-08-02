243105907d64aeeb7e82f9f6fecd0a6e74261dff
YangWen <yangwen99@163.com>
2013-08-02 15:17:29 +0800

    新建程序
    utf8为服务端，gbk为客户端

---
+++ server/source/plugin/yiqixueba/admincp.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+	exit('Access Denied');
+}
+
+require_once DISCUZ_ROOT.'source/plugin/'.$plugin['directory'].'function.func.php';
+
+?>
